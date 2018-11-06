4fd819990414ef92a501d50c4b27e303d8cbaede
tennie-jacobson <tennie.jacobson@outlook.com>
2018-11-05 20:55:10 -0600

    Fixed the inline issue after the user validates form.

--- restaurant-site/scripts/restaurant-contact.js
+++ restaurant-site/scripts/restaurant-contact.js
@@ -2,7 +2,7 @@ function clearErrors() {
   for(var i = 0; i < document.forms["userInquiry"].elements.length; i++) {
     if(document.forms["userInquiry"].elements[i]
           .parentElement.className.indexOf("has-") != -1) {
-      document.forms["userInquiry"].elements[i].parentElement.className = "form-group";
+      document.forms["userInquiry"].elements[i].parentElement.className = "form-group form-inline";
     }
   }
 }
@@ -16,19 +16,19 @@ function validateForm() {
 
   if(userName == isNaN(userName) || userName == "") {
     alert("Name is a required field.");
-    document.forms["userInquiry"]["userName"].parentElement.className = "form-group has-error";
+    document.forms["userInquiry"]["userName"].parentElement.className = "form-group form-inline has-error";
     document.forms["userInquiry"]["userName"].focus();
     return false;
   }
   if(userEmail == isNaN(userEmail) || userEmail == "") {
     alert("Email is a required field.");
-    document.forms["userInquiry"]["userEmail"].parentElement.className = "form-group has-error";
+    document.forms["userInquiry"]["userEmail"].parentElement.className = "form-group form-inline has-error";
     document.forms["userInquiry"]["userEmail"].focus();
     return false;
   }
   if(userPhone == isNaN(userPhone) || userPhone == "") {
     alert("Phone number is a required field.");
-    document.forms["userInquiry"]["userPhone"].parentElement.className = "form-group has-error";
+    document.forms["userInquiry"]["userPhone"].parentElement.className = "form-group form-inline has-error";
     document.forms["userInquiry"]["userPhone"].focus();
     return false;
   }
